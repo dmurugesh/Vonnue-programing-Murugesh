fef8b2015e60699e8209b9a1780139f5b4b72cfb
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-13 08:25:02 +0530

    checked Primenumber

---
+++ jsbasics/primenumber.js
@@ -0,0 +1,29 @@
+let readline = require("readline-sync");
+let num = readline.question("Enter the NUmber"); 
+
+const testPrime = num =>
+{
+  if (num === 1)   // if num = 1 it is not a prime
+  {
+    return false;  //break  false 
+  }
+
+  else if(num === 2)  // if num = 2 it is prime 
+   {
+    return true;  
+   }
+
+  else
+    {
+     for(let x = 2; x < num; x++)    // 
+     {
+       if(num % x === 0)
+       {
+         return false;
+       }
+     }
+     return true;  
+    }
+ }
+
+console.log('is the number Prime? ' , testPrime(num));
