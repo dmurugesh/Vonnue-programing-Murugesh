7cd2882c92477685aa2adedffd742bfc68267a4f
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-14 23:29:44 +0530

    simpleprograms 1 to 9

---
+++ simpleprograms/001_printarray.js
@@ -0,0 +1,11 @@
+let readline = require("readline-sync");
+let size = readline.question ("Enter the No. of element to store in Array: ");
+
+const inputArray = [];
+for (i=0; i<size; i++) {
+    inputArray[i] = readline.question("Enter the Element " + (i+1) + " : ");
+}
+console.log(inputArray);
+
+
+
---
+++ simpleprograms/003_replace.js
@@ -0,0 +1,15 @@
+let readline = require("readline-sync");
+let size = readline.question("Enter the size of Array : ");
+let inputArray = [];
+
+for (i =0; i<size; i++) {
+ inputArray[i] = readline.question("Enter the " + (i+1) + "Elment: ");
+ }
+console.log("Enter Array :" + inputArray);            // used to multiple each elment in array and 
+inputArray.forEach((value, index) => {
+    inputArray[index] *= 0;
+});
+console.log(inputArray);
+
+
+
