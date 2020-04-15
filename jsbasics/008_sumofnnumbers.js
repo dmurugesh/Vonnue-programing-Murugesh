ae7139b33ed0ee5e9b88a989cc0bb1f8cb78a5d5
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-15 22:43:33 +0530

    08_Sumofnnumbers

---
+++ jsbasics/008_sumofnnumbers.js
@@ -0,0 +1,12 @@
+let readline = require("readline-sync");
+let n = readline.question("Enter the No. of element: ");
+
+const findSum = num => {
+    let result = 0;
+    for (let i = 1; i <= num; i++) {
+            result = result + i;
+    }
+    return result;
+}
+findSum(n)
+console.log('The sum of N elements :' + findSum(n))
