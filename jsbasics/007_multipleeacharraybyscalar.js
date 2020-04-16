570c333bdb07d721677c27bcdc3f38831fffa6a0
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-04-16 22:22:13 +0530

    Update 007_multipleeacharraybyscalar.js

--- jsbasics/007_multipleeacharraybyscalar.js
+++ jsbasics/007_multipleeacharraybyscalar.js
@@ -1,16 +1,20 @@
+// Question 7 => Multiple each element of an array with a constant and print the result ?
 let readline = require("readline-sync");
 let inputArray = [];
 
 const enterArrayElements = array => {
-    let size = readline.question("Enter the size of Array : ");
-    for (i = 0; i < size; i++) {
-        inputArray[i] = readline.question("Enter the " + (i + 1) + "Elment: ");
+    let size = readline.question("Enter the No. of element to store in Array: ");
+    for (let i = 0; i < size; i++) {
+        array[i] = readline.question("Enter the Element " + (i + 1) + " : ");
     }
-    console.log("Enter Array :" + inputArray);
+    console.log(array);
+}
+
+const squareArray = array => {
     let scalar = readline.question("Enter number to be multiplied with each elemnt :");
-    inputArray.forEach((value, index) => {
-        inputArray[index] = inputArray[index] *  scalar;         // used to multiple each elment in array with itself
-    });
-    console.log(inputArray);
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i] * scalar);
+    }
 }
 enterArrayElements(inputArray);
+squareArray(inputArray);            
