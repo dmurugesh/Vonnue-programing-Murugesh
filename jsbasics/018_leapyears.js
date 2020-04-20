136c2369de7c651598072e0bf5fdb2a3534c0b37
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-20 21:21:06 +0530

    leapyear

---
+++ jsbasics/018_leapyears.js
@@ -0,0 +1,12 @@
+// Question 18 =>  Write a program that prints the next 20 leap years.
+// num is defined as the present year
+
+leapYear = num => {           
+    let upto = parseInt(num) + 20;
+    for (let i = num; i <= upto; i++) {
+        if (i % 4 === 0) {
+            console.log("Leap year : " + i);
+        }
+    }
+}
+leapYear(2020);
