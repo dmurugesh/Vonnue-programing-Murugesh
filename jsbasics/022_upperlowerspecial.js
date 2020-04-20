f4cb0bb3c110a2f2c715ed580d7fdfe415f3eb4f
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-04-20 21:35:56 +0530

    Update 022_upperlowerspecial.js

--- jsbasics/022_upperlowerspecial.js
+++ jsbasics/022_upperlowerspecial.js
@@ -5,8 +5,9 @@ theinputCharacter = str => {
         console.log("The entered string" + " '" + str + "' " + "is a Uppercase");
     } else if (str.match(/[a-z]/)) {
         console.log("The entered string" + " '" + str + "' " + "is a lowercase");
-    }
-    else {
+    } else if (str.match(/[0-9]/)) {
+        console.log("The entered string" + " '" + str + "' " + "is a number");
+    } else {
         console.log("The entered string" + " '" + str + "' " + "is a special Character");
     }
 }
