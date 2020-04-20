735393f32b2847df6b434eab6a296989ed45d24d
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-20 21:30:03 +0530

    alphabet

---
+++ jsbasics/020_alphabetornot.js
@@ -0,0 +1,15 @@
+// Question 20 => Write a program to check if a character is an alphabet or not ?
+// word.match(/[A-Z a-z]/) used to check captial//small letter alphabet
+// word.match(/[0-9]/) used to check the numbers in the string
+
+theinputCharacter = word => {
+    if (word.match(/[0-9]/)) {
+        console.log("The character" + " '" + word + "' " + "is a Number");
+    } else if (word.match(/[A-Z a-z]/)) {
+        console.log("The character" + " '" + word + "' " + "is a alphabet");
+    } else {
+        console.log("The character" + " '" + word + "' " + "is a special Character");
+    }
+}
+theinputCharacter("b");
+
