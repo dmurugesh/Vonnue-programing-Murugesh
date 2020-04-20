0d1b83a507e2e12cea136081c1093a95cf695b4a
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-20 21:33:22 +0530

    uppercase

---
+++ jsbasics/022_upperlowerspecial.js
@@ -0,0 +1,13 @@
+// Question 22 => Write a program to check Uppercase, Lowercase or special character ?
+
+theinputCharacter = str => {
+    if (str.match(/[A-Z]/)) {
+        console.log("The entered string" + " '" + str + "' " + "is a Uppercase");
+    } else if (str.match(/[a-z]/)) {
+        console.log("The entered string" + " '" + str + "' " + "is a lowercase");
+    }
+    else {
+        console.log("The entered string" + " '" + str + "' " + "is a special Character");
+    }
+}
+theinputCharacter("Anystring");
