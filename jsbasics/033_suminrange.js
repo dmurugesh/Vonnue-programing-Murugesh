51784f2ec49e14604e60851a6fcd488d5d955fcf
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-22 22:24:24 +0530

    review comments

--- jsbasics/033_suminrange.js
+++ jsbasics/033_suminrange.js
@@ -3,11 +3,11 @@
 sumNumber = (num1, num2) => {
     let sum = 0;
     if (num2 > num1) {
-        for (i = num1; i <= num2; i++) {
+        for (let i = num1; i <= num2; i++) {
             sum = (sum + i);
         }
     } else {
-        for (i = num2; i <= num1; i++) {
+        for (let i = num2; i <= num1; i++) {
             sum = (sum + i);
         }
     }
