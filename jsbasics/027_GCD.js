249410dd6d29e4df9e4201bfdaef64d6e383aa2c
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-22 22:22:14 +0530

    reviewcomments

--- jsbasics/027_GCD.js
+++ jsbasics/027_GCD.js
@@ -3,20 +3,20 @@
 findGcd = (num1, num2) => {
     let gcd = 0;
     if (num1 > num2) {
-    for (let i = 1; i <= num2; i++) {
-        if (num1 % i === 0 && num2 % i === 0) {
-           gcd = i * 1;
+        for (let i = 1; i <= num2; i++) {
+            if (num1 % i === 0 && num2 % i === 0) {
+                gcd = i * 1;
+            }
         }
-    } 
-    return gcd;
-} else { 
-    for (let i = 1; i <= num1; i++) {
-        if (num1 % i === 0 && num2 % i === 0) {
-           gcd = i * 1;
+        return gcd;
+    } else {
+        for (let i = 1; i <= num1; i++) {
+            if (num1 % i === 0 && num2 % i === 0) {
+                gcd = i * 1;
+            }
         }
-    } 
-    return gcd;
-}
+        return gcd;
+    }
 }
 result = findGcd(180, 120);
 console.log('The LCM of both the numbers are : ' + result);
