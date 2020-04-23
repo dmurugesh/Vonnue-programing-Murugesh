e72ed5b353d57cfe07b5016546c109ca776ee694
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-04-23 22:37:06 +0530

    Update 011_multiplicationtable.js

--- jsbasics/011_multiplicationtable.js
+++ jsbasics/011_multiplicationtable.js
@@ -3,7 +3,7 @@
 const multiplicationTable = () => {
   for (let i = 1; i <= 12; i++) {
     for (let j = 1; j <= 10; j++) {
-      result = i * j;
+      let result = i * j;
       console.log(i + ' * ' + j + ' = ' + result);
     }
     console.log('Multiplication table of ' + (i + 1));
