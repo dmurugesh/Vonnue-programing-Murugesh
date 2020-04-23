f3a0ac5e745ee4d1985e1e064cf6fd47e1a8014b
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-23 22:08:14 +0530

    reverseanumber

---
+++ jsbasics/034_reverseanumber.js
@@ -0,0 +1,12 @@
+// Question 34 => Reverse a given number
+
+inputNumber = num => {
+    let temp = 0;
+    while (num > 0) {
+        let reminder = num % 10;
+        num = parseInt(num / 10);
+        temp = ((temp * 10) + reminder);
+    }
+    console.log('reversed number is : ' + temp);
+}
+inputNumber(1234);  
