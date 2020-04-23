b5c3a912f3b8829bb21b8d8d2034f8322609fdd4
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-23 22:29:20 +0530

    powerofn

---
+++ jsbasics/041_powerofNumber.js
@@ -0,0 +1,8 @@
+// Question 41 => Find the power of a number
+
+power = (num, expo) => {
+    let result = (num ** expo);
+    return result;
+}
+let answer = power (4, 2);
+console.log('final Value : ' + answer);
