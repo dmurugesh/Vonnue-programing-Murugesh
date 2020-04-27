8cc0246360ba937762a7e92c0e597308d940e7c9
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-27 21:47:23 +0530

    binarytodecimal

---
+++ jsbasics/048_binarytodecimal.js
@@ -0,0 +1,15 @@
+// Question 48. Binary to decimal conversion ?
+//below is radix table used for conersion used for conversion any binary/octal/hexa to decimal value
+/* Radix	Name
+    2	    Binary numeral system
+    8	    Octal system
+    10	    Decimal system
+    16	    Hexadecimal system 
+let dec = parseInt(num, 2).toString(10); where (num, 2) is the radix(2) as input & .tostring(radix) enter the radix value to converted
+*/
+
+binarytoDecimal = num => {
+     let dec = parseInt(num, 2).toString(10);
+     console.log(dec); 
+}
+binarytoDecimal(1001);
