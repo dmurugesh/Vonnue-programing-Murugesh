2da0a59dfbbf9cf9bb4b3fb54d13e245c1895668
dmurugesh <dmurugeshkhanna@gmail.com>
2020-04-29 22:27:53 +0530

    sample

--- jsbasics/064_reversealist.js
+++
@@ -1,9 +0,0 @@
-// Question 64. Write function that reverses a list, preferably in place. ?
-
-reverseList = array => {
-    var output = [].concat(array).reverse();
-     console.log(output);
-}
-
-let input = ['0', '4', '89', '55'];
-reverseList(input);
