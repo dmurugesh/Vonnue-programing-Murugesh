8a1ffd19e807e9e63d17a75a7dab5e04f7be1780
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-05-05 11:18:24 +0530

    Update primenumber.js

--- jsbasics/primenumber.js
+++ jsbasics/primenumber.js
@@ -4,11 +4,11 @@ let num = readline.question("Enter the NUmber");
 testPrime = num => {
   if (num === 1)  {    // if num = 1 it is not a prime 
     return false;  //break  false 
-  } else if(num === 2) {
+  } else if (num === 2) {
     return true;  
    } else {
-     for(let x = 2; x < num; x++)  {
-       if(num % x === 0) {
+     for (let x = 2; x < num; x++)  {
+       if (num % x === 0) {
          return false;
        }
      }
