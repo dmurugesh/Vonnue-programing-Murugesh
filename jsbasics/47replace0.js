9acec49fd1690de511d0d65da7ba3445879c0f6c
dmurugesh <dmurugeshkhanna@gmail.com>
2020-05-06 11:09:52 +0530

    reviewcomments commited

--- jsbasics/47replace0.js
+++ jsbasics/47replace0.js
@@ -1,16 +1,19 @@
 // Queation 47 => Replace all 0's with 1 in a given integer ?
 
-replaceNumber = num => {
+replaceDigit = num => {
+    let sum = 0;
     let result = 0;
-    while (num > 0) {
-        let reminder = num % 10;
-            if (reminder === 0) {
-                let temp = 1;
-                reminder = (reminder + temp);
-            }
-        num = parseInt(num / 10);
+    while (num >= 1) {
+        reminder = num % 10;
+        if (reminder === 0) {
+            let temp = 1;
+            reminder = (reminder + temp);
+        }
+        num = Math.floor(num / 10);
         result = ((result * 10) + reminder);
     }
     console.log(result);
 }
-replaceNumber(01025);
+
+replaceDigit(505);
+
