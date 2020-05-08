3e3a604b8a4ff60b791f9dfdbfa5888a374b3f9e
dmurugesh <dmurugeshkhanna@gmail.com>
2020-05-08 18:49:52 +0530

    comments

--- jsbasics/044_primenumberingivnrange.js
+++ jsbasics/044_primenumberingivnrange.js
@@ -1,28 +1,18 @@
 // Question 44 => Prime numbers in a given range ?
 
 primeNumber = (num1, num2) => {
-    if (num1 > num2) {
-        for (let i = num2; i <= num1; i++) {
+    let largest = num1 > num2 ? num1 : num2; 
+    let smallest = num1 < num2 ? num1 : num2; 
+    for (let i = smallest; i <= largest; i++) {
             let isPrime = true;
-            for (let j = 2; j < i; j++) {
-                if (i % j === 0) {
-                    isPrime = false;
-                    break;
-                }
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
             }
-            if (isPrime) console.log(i);
-        }
-    } else {
-        for (let i = num1; i <= num2; i++) {
-            let isPrime = true;
-            for (let j = 2; j < i; j++) {
-                if (i % j === 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            if (isPrime) console.log(i);
-        }
+        } 
+        if (isPrime)  console.log(i); 
     }
 }
+
 primeNumber(20, 10);
