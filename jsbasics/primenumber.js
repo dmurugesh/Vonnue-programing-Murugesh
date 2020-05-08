618af255b1ea5e199df672c3e9561a30ad5afe1a
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-05-08 13:17:22 +0530

    Update primenumber.js

--- jsbasics/primenumber.js
+++ jsbasics/primenumber.js
@@ -2,17 +2,18 @@ let readline = require("readline-sync");
 let num = readline.question("Enter the NUmber"); 
 
 testPrime = num => {
-  if (num === 1)  {    // if num = 1 it is not a prime 
-    return false;  //break  false 
-  } else if (num === 2) {
-    return true;  
-   } else {
-     for (let x = 2; x < num; x++)  {
-       if (num % x === 0) {
-         return false;
-       }
-     }
-     return true;  
+    if (num === 1) {    // if num = 1 it is not a prime 
+        return false;  //break  false 
+    } else if (num === 2) {
+        return true;
+    } else {
+        for (let x = 2; x < num; x++) {
+            if (num % x === 0) {
+                return false;
+            }
+        }
+        return true;
     }
- }
-console.log('is the number Prime? ' , testPrime(num));
+}
+
+console.log('is the number Prime? ', testPrime(num));
