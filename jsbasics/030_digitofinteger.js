19f2c440bf137c92de84f64d5a4cd3754fe9b43d
dmurugesh <63349799+dmurugesh@users.noreply.github.com>
2020-05-08 13:43:56 +0530

    Update 030_digitofinteger.js

--- jsbasics/030_digitofinteger.js
+++ jsbasics/030_digitofinteger.js
@@ -1,14 +1,12 @@
 // Question 30 => Number of digits in an integer ?
 
 integerCount = num => {
-    let count = 0;
-    if (num >= 1) {
-    count++; 
-  }
+    let count = 1;
     while (num / 10 >= 1) {
       num = (num / 10);
       count++;
     }
     return count;
   }
+
   console.log(integerCount(12112));
